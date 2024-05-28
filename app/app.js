99474667c8ad92c3f4e3122b5738a8ddcfc4f166
“Taiseer <thussein@redhat.com>
2024-05-28 19:14:10 +0000

    updated my app

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Baseline Demo Phase 2!');
+  res.send('Baseline Demo Phase 2! demo test');
 });
 
 app.listen(8080, function () {
